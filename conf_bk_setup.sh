3d656dec3a15d8ecf6a746799e0865293a02b9a7
NetDev <netdev@NetDevs-MBP.hsd1.fl.comcast.net>
2020-04-11 18:53:00 -0400

    commit

--- conf_bk_setup.sh
+++ conf_bk_setup.sh
@@ -30,7 +30,7 @@ apt-get install libsystemd-dev
 # Install all the python requirements for the root user since he will be running the logger tool
 sudo -H /usr/bin/pip3 install -r ~/cisco-conf-log/requirements.txt --user
 # Install all the python requirements for the user running the command
-$(which python3) -m pip install -r ~/cisco-conf-log/requirements.txt --user
+/usr/bin/pip3 install -r ~/cisco-conf-log/requirements.txt
 echo "###########################################################"
 echo "System Requirement and Python Libs are Installed"
 echo "###########################################################"
