09bb7a38877e01671eaed46ac0b9b947ac87a037
NetDev <netdev@NetDevs-MBP.hsd1.fl.comcast.net>
2020-04-12 12:10:01 -0400

    commit

--- conf_bk/setup.py
+++ conf_bk/setup.py
@@ -9,14 +9,13 @@ def get_schedule():
         parse_data = yaml.load(file, Loader=yaml.FullLoader)
     return parse_data['schedules']
 
-        
-#crontab = open("/var/spool/cron/crontabs/root","w+")
 
 schedules = get_schedule()
 
 directory = os.getcwd()
 
 cron = CronTab(user='root')
+cron.remove_all()
 
 for entry in schedules:
     site = entry['site_name']
@@ -26,12 +25,3 @@ for entry in schedules:
     job.setall(time)
     cron.write()
 
-#for entry in schedules:
-#    site = entry['site_name']
-#    time = entry['time']
-#    crontab.write("\n")
-#    crontab.write("#Job to backup the configuratios for site {}\n".format(site))
-#    crontab.write('{time} cd {dir}/conf_bk && $(which python3) main.py {site}'.format(time=time,dir=directory,site=site))
-#    crontab.write("\n")
-
-#crontab.close()
