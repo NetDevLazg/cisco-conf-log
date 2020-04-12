1e9b52852688206cbf8fa3bd1108157c5c89bbdb
NetDev <netdev@NetDevs-MBP.hsd1.fl.comcast.net>
2020-04-11 20:13:52 -0400

    creating syslog-ng files

---
+++ syslog_ng_setup.sh
@@ -0,0 +1,55 @@
+#---------------------------------------------------------------------------------------------------------
+# Copy the folder into the /op/ directory in order to create the service in systemd
+echo "###########################################################"
+echo "Installing Syslog-NG Service"
+echo "###########################################################"
+sleep 2
+# Install the tool syslog-ng
+apt-get install syslog-ng -y
+#
+sleep 1
+echo '
+source cisco_network {
+       udp(port(514) flags(no-parse));
+};
+
+source nexus_network {
+       udp(port(518));
+};
+
+filter config_change { match("Configured" value("MESSAGE")) };
+
+parser cisco_parser {
+    cisco-parser (prefix(.SDATA.my-parsed-data.));
+};
+
+log {
+    source(cisco_network);
+    parser(cisco_parser);
+    destination(d_tcp);
+};
+
+log {
+    source(nexus_network);
+    destination(d_tcp);
+};
+
+
+log {
+    source(cisco_network);
+    filter(config_change);
+    parser(cisco_parser);
+    destination(python);
+};
+
+log {
+    source(nexus_network);
+    filter(config_change);
+    destination(python);
+};
+
+destination python { syslog("0.0.0.0" transport("tcp") port(10000)); };
+' > /etc/syslog-ng/conf.d/cisco.conf
+
+# Restarts Syslog-NG
+systemctl restart syslog-ng
